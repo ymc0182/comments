64df6dacc7cd4505226b1a27dcdea2645be32653
ymc0182 <501414437@qq.com>
2025-04-24 19:01:10 +0800

    init

--- src/utils/getPublicationDays.ts
+++
@@ -1,8 +0,0 @@
-export function getPublicationDays(puDate: Date, upDate?: Date){
-  const dateToCompare = upDate || puDate;
-  const currentDate = new Date();
-  const timeDifference = currentDate.getTime() - dateToCompare.getTime();
-  const daysDifference = timeDifference / (1000 * 3600 * 24);
-
-  return Math.floor(daysDifference);
-}
